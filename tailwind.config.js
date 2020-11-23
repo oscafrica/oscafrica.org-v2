1abc8a65f53ec0dc9667b8fa8bc7b152d3715aed
alabobriggs <alabobriggs@gmail.com>
2020-11-23 11:50:36 +0100

    add blue and orange color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,23 +3,33 @@
 module.exports = {
   theme: {
     fontFamily: {
-      display: ["source sans", "avenir", "gotham", "ubuntu"],
-      body: ["source sans", "avenir", "gotham", "ubuntu"],
-      avenir: ["avenir"],
-      ubuntu: ["ubuntu"]
+      primary: ["Work Sans", "sans-serif"],
+      secondary: ["Cousine", "sans-serif"]
     },
-    prefix: "oc-",
     extend: {
-      spacing: {
-        "oc-10": "10px"
-      },
-      height: {
-        "half-screen": "50vh"
-      },
       colors: {
         white: "#FFFFFF",
-        "dark-blue": {
-          primary: "#081E31",
+        orange: {
+          100: "#FCF6ED",
+          200: "#FBEDDA",
+          300: "#F6D9B6",
+          400: "#F4C894",
+          500: "#F9AA4F",
+          600: "#D97D13",
+          700: "#AF630C",
+          800: "#804809"
+        },
+        blue: {
+          100: "#E1EBF4",
+          200: "#A9C3D9",
+          300: "#6388A8",
+          400: "#4C6F8E",
+          500: "#43637F",
+          600: "#33516B",
+          700: "#25435D",
+          800: "#16334C"
+        },
+        jonquil: {
           100: "#96ADDD",
           200: "#5A75AB",
           300: "#445984",
@@ -27,28 +37,14 @@ module.exports = {
           500: "#283756",
           600: "#283757"
         },
-        orange: {
-          primary: "#F79313",
-          100: "#FFE0BC",
-          200: "#F5C388",
-          300: "#F9AA4F",
-          400: "#F7931E"
-        },
-        gray: {
-          primary: "464B54",
-          100: "#F0F0F0",
-          200: "#D6D6D6"
-        },
-        "light-green": {
-          primary: "#F1F2D3",
-          100: "#FCFDED",
-          200: "#D7D9B4"
-        },
-        "dark-green": "#B2B396",
-        "trans-gray": "rgba(10, 34, 55, 0.5)",
-        "circle-gray": "#E7ECF8",
-        brown: "#440101",
-        "white-smoke": "#F4F4F4"
+        yellow: {
+          100: "#96ADDD",
+          200: "#5A75AB",
+          300: "#445984",
+          400: "#283756",
+          500: "#283756",
+          600: "#283757"
+        }
       },
       fontSize: {
         base: "1.4rem",
@@ -64,13 +60,6 @@ module.exports = {
         body: "1.6rem",
         "small-text": "1.4rem",
         captions: "1.2rem"
-      },
-      lineHeight: {
-        text: "22px"
-      },
-      container: {
-        padding: "5rem",
-        center: true
       }
     }
   },
