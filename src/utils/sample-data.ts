3befca968c91ce0d0973302aa0b6750e509c5a5c
alabobriggs <alabobriggs@gmail.com>
2020-11-23 11:36:47 +0100

    move folders to src

--- src/utils/sample-data.ts
+++ src/utils/sample-data.ts
@@ -5,5 +5,5 @@ export const sampleUserData: User[] = [
   { id: 101, name: 'Alice' },
   { id: 102, name: 'Bob' },
   { id: 103, name: 'Caroline' },
-  { id: 104, name: 'Dave' },
+  { id: 103, name: 'Dave' },
 ]
