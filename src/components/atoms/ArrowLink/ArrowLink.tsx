439f30e389534f47c4e6e92a4ace071987c051ea
alabobriggs <alabobriggs@gmail.com>
2020-12-11 23:50:47 +0100

    add arrow link

---
+++ src/components/atoms/ArrowLink/ArrowLink.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface ArrowLinkProps {
+  href: string;
+}
+
+const ArrowLink: React.FC<ArrowLinkProps> = ({ children, href }) => {
+  return (
+    <a href={href} target="_blank" rel="noreferrer noopener" className="flex items-center">
+      <span className="mr-4 font-medium inline-block text-white">{children}</span>
+      <img alt="arrow-right" src="/images/icons/util/right-arrow.svg" />
+    </a>
+  );
+};
+
+export type { ArrowLinkProps };
+
+export default ArrowLink;
