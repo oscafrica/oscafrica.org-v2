961dcd86ba2ff6b1c863b84bb381c8b53ee29049
alabobriggs <alabobriggs@gmail.com>
2020-12-12 18:49:18 +0100

    add section title size condition

--- src/components/atoms/Typography/Headings/SectionTitle/SectionTitle.tsx
+++ src/components/atoms/Typography/Headings/SectionTitle/SectionTitle.tsx
@@ -3,12 +3,24 @@ import classNames from "classnames";
 
 interface SectionTitleProps {
   color?: "black" | "white";
+  pageHeading?: boolean;
   children: any;
   className?: string;
 }
 
-const SectionTitle: React.FC<SectionTitleProps> = ({ children, className, color = "black" }: SectionTitleProps) => {
-  return <h2 className={classNames("text-h5 font-bold tracking-widest", `text-${color}`, className)}>{children}</h2>;
+const SectionTitle: React.FC<SectionTitleProps> = ({ children, className, color = "black", pageHeading }) => {
+  return (
+    <h2
+      className={classNames(
+        "font-bold tracking-widest",
+        pageHeading ? "text-h3" : "text-h5",
+        `text-${color}`,
+        className
+      )}
+    >
+      {children}
+    </h2>
+  );
 };
 
 export type { SectionTitleProps };
