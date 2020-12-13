85b5b3405bfca70c02a6ef8b513f66fbd4620fb6
alabobriggs <alabobriggs@gmail.com>
2020-12-13 20:05:26 +0100

    add toggle color mode on arrow link

--- src/components/atoms/ArrowLink/ArrowLink.tsx
+++ src/components/atoms/ArrowLink/ArrowLink.tsx
@@ -1,16 +1,26 @@
 import Link from "next/link";
+import classNames from "classnames";
 import React from "react";
 
 interface ArrowLinkProps {
   href: string;
+  colored?: boolean;
 }
 
-const ArrowLink: React.FC<ArrowLinkProps> = ({ children, href }) => {
+const ArrowLink: React.FC<ArrowLinkProps> = ({ children, href, colored }) => {
   return (
     <Link href={href}>
       <a className="flex items-center">
-        <span className="mr-4 font-medium inline-block text-white">{children}</span>
-        <img alt="arrow-right" className="w-5" src="/images/icons/util/right-arrow.svg" />
+        <span
+          className={classNames("mr-4 font-medium inline-block text-white", colored ? "text-primary" : "text-white")}
+        >
+          {children}
+        </span>
+        <img
+          alt="arrow-right"
+          className="w-5"
+          src={colored ? "/images/icons/util/arrow-colored.svg" : "/images/icons/util/arrow-white.svg"}
+        />
       </a>
     </Link>
   );
