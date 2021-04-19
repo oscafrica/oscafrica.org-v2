f7791171f083cdb13b067190c5f11c06fab36146
alabobriggs <alabobriggs@gmail.com>
2021-04-19 15:36:07 +0100

    make index page responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,28 +19,27 @@ const IndexPage = () => {
   return (
     <>
       <SEOHead />
-      <div className="bg-black min-h-screen max-h-screen overflow-hidden flex flex-col">
+      <div className="bg-black lg:min-h-screen lg:max-h-screen overflow-hidden flex flex-col">
         <NavBar tune="light" />
 
-        <main className="flex min-h-screen -mt-28 flex-grow">
-          <SectionWithBackground background="bg-1" className="w-2/3">
-            <div className="min-h-full bg-secondary hover:bg-opacity-80 bg-opacity-50 w-full pl-32 flex flex-col justify-end items-start pb-72">
-              <SectionTitle color="white" fontSize="40">
+        <main className="flex flex-col lg:flex-row min-h-screen lg:-mt-28 flex-grow">
+          <SectionWithBackground background="bg-1" className="w-full h-full lg:h-auto lg:w-2/3">
+            <div className="min-h-full bg-secondary pt-96 lg:pt-0 hover:bg-opacity-80 bg-opacity-50 w-full px-10 lg:pl-32 flex flex-col justify-end items-start pb-48 lg:pb-72">
+              <SectionTitle color="white" fontSize="40" className="pt-16 lg:pt-0">
                 The Next Billion Creators
               </SectionTitle>
-              <p className="text-white font-medium text-paragraph-2 mb-16 mt-8">
-                A community of creative minds driving the Open Source
-                <br /> movement in Africa.
+              <p className="text-white font-medium lg:w-1/2 text-paragraph-2 mb-16 mt-8">
+                A community of creative minds driving the Open Source movement in Africa.
               </p>
               <Link href="/about-us">
-                <a className="bg-white font-medium hover:bg-primary rounded-primary duration-200 transition-all py-6 px-10">
+                <a className="bg-white hidden lg:inline-block font-medium hover:bg-primary rounded-primary duration-200 transition-all py-6 px-10">
                   Who we are
                 </a>
               </Link>
             </div>
           </SectionWithBackground>
 
-          <section className="w-1/3 flex flex-col">
+          <section className="w-full h-screen lg:h-auto lg:w-1/3 flex flex-col">
             <SectionWithBackground background="bg-2" className="flex-grow">
               <div className="flex flex-col bg-blue-500 bg-opacity-30 hover:bg-opacity-80 h-full justify-end px-16 pb-16">
                 <SectionTitle color="white" fontSize="28">
