b82e8c3182668b33e79983fa27649d521a24e1d4
alabobriggs <alabobriggs@gmail.com>
2021-05-30 12:53:53 +0100

    fix link

--- src/components/atoms/ArrowLink/ArrowLink.tsx
+++ src/components/atoms/ArrowLink/ArrowLink.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import classNames from "classnames";
 import React from "react";
+import styled from "styled-components";
 
 interface ArrowLinkProps {
   href: string;
@@ -9,22 +10,31 @@ interface ArrowLinkProps {
 
 const ArrowLink: React.FC<ArrowLinkProps> = ({ children, href, colored }) => {
   return (
-    <Link href={href}>
-      <a className="flex items-center">
+    <Link href={href} passHref>
+      <StyledLink className="inline-flex items-center pb-4  hover:border hover:border-b hover:border-white">
         <span
-          className={classNames("mr-4 font-medium inline-block text-white", colored ? "text-primary" : "text-white")}
+          className={classNames("mr-4 text-base font-medium inline-block text-white", colored ? "text-primary" : "text-white")}
         >
           {children}
         </span>
         <img
           alt="arrow-right"
-          className="w-5"
+          className="w-5 duration-200"
           src={colored ? "/images/icons/util/arrow-colored.svg" : "/images/icons/util/arrow-white.svg"}
         />
-      </a>
+      </StyledLink>
     </Link>
   );
 };
+
+const StyledLink = styled.a`
+  border-bottom: 1px solid transparent;
+  &:hover {
+    img {
+      margin-left: 10px;
+    }
+  }
+`;
 
 export type { ArrowLinkProps };
 
