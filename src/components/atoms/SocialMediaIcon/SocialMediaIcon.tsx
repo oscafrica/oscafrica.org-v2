714615132a9fe521aac60ec21d74700245e441c5
alabobriggs <alabobriggs@gmail.com>
2021-07-07 13:39:51 +0100

    add hover effect on image

--- src/components/atoms/SocialMediaIcon/SocialMediaIcon.tsx
+++ src/components/atoms/SocialMediaIcon/SocialMediaIcon.tsx
@@ -10,7 +10,7 @@ interface SocialMediaIconProps {
 const SocialMediaIcon: React.FC<SocialMediaIconProps> = ({ social, href, className }) => {
   return (
     <a href={href} className={className} target="_blank" rel="noreferrer noopener">
-      <img alt={social} src={`/images/icons/social/footer/${social}.svg`} />
+      <img alt={social} className="transform duration-200 hover:scale-110" src={`/images/icons/social/footer/${social}.svg`} />
     </a>
   );
 };
