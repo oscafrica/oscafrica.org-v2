0673e8f8b8afccdfdcafc9a844bce7101355a905
alabobriggs <alabobriggs@gmail.com>
2021-07-15 23:55:01 +0100

    add analytics

---
+++ src/utils/analytics.ts
@@ -0,0 +1,26 @@
+import ReactGA from "react-ga";
+
+export const initGA = () => {
+  ReactGA.initialize(`${process.env.GOOGLE_ANALYTICS}`);
+};
+
+export const logPageView = () => {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname + window.location.search);
+};
+
+export const logEvent = (category = "", action = "", label = "") => {
+  if (category && action && label) {
+    ReactGA.event({
+      category,
+      action,
+      label
+    });
+  }
+};
+
+export const logException = (description = "", fatal = false) => {
+  if (description) {
+    ReactGA.exception({ description, fatal });
+  }
+};
