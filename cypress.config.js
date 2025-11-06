e06b3fd372bb82355a2ea4b6d2df836cb1a1a00c
Bolaji Ayodeji <sirbeejay1@gmail.com>
2025-11-06 13:24:11 +0100

    chore: pause ci actions

---
+++ cypress.config.js
@@ -0,0 +1,8 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}"
+  }
+});
