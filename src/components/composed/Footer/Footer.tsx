9a67d9f52fdb9aa04f34b2e95157accd50350794
Bolaji Ayodeji <sirbeejay1@gmail.com>
2026-05-05 15:52:23 +0100

    Update copyright year in Footer component
    Updated copyright year to the current year dynamically.

--- src/components/composed/Footer/Footer.tsx
+++ src/components/composed/Footer/Footer.tsx
@@ -114,11 +114,7 @@ const Footer = () => {
       </ul>
 
       <p className="w-full mt-8 md:mt-12 text-center md:text-left text-black font-bold opacity-80 text-base">
-        This site is powered by{" "}
-        <a href="https://netlify.com?ref=oscafrica" target="_blank" rel="noopener noreferrer">
-          Netlify
-        </a>
-        &nbsp;| Copyright &copy; Open Source Community Africa 2025.
+        Copyright &copy; Open Source Community Africa {new Date().getFullYear()}.
       </p>
     </header>
   );
